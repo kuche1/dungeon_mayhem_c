9c6d681ac7857673c8356d5ee6b35ff169018f1c
kuche1 <znamsiparolata4esno@abv.bg>
2025-02-01 04:52:15 +0200

    update

--- dungeon_mayhem.c
+++ dungeon_mayhem.c
@@ -24,33 +24,32 @@ int main(void)
         return 1;
     }
 
-    struct buf clients;
-    buf_init(& clients, sizeof(struct client));
+    buf_init(& g_ctx.clients, sizeof(struct client));
 
     int clients_update_eventfd = eventfd(0, EFD_NONBLOCK);
     if(clients_update_eventfd < 0){
         perror("ERROR: eventfd");
-        buf_deinit(& clients);
+        buf_deinit(& g_ctx.clients);
         net_deinit(& net);
         return 1;
     }
 
     pthread_t cht_thr;
-    if(client_handler_thread_spawn(& g_ctx, & cht_thr, & clients, clients_update_eventfd)){
+    if(client_handler_thread_spawn(& g_ctx, & cht_thr, & g_ctx.clients, clients_update_eventfd)){
         close(clients_update_eventfd);
-        buf_deinit(& clients);
+        buf_deinit(& g_ctx.clients);
         net_deinit(& net);
         return 1;
     }
 
-    accept_connections_loop(& g_ctx.shutting_down, & net, & clients, clients_update_eventfd);
+    accept_connections_loop(& g_ctx.shutting_down, & net, & g_ctx.clients, clients_update_eventfd);
 
     client_handler_thread_join(cht_thr);
 
     close(clients_update_eventfd);
 
     // TODO close all client connections?
-    buf_deinit(& clients);
+    buf_deinit(& g_ctx.clients);
 
     net_deinit(& net);
 
--- dungeon_mayhem.h
+++ dungeon_mayhem.h
@@ -1,9 +1,12 @@
 #ifndef DUNGEON_MAYHEM_H
 #define DUNGEON_MAYHEM_H
 
+#include "buf.h"
+
 struct global_context
 {
     int shutting_down;
+    struct buf clients;
 };
 
 int main(void);
